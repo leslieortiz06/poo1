f3581c8fc35166d8a7fc110d6ac215d669c23fab
leslieortiz06 <oleslienathali@gmail.com>
2024-11-13 13:06:21 -0600

    actividad 24

--- p145_EstudianteGUI_V1/App.java
+++ p145_EstudianteGUI_V1/App.java
@@ -21,6 +21,7 @@ public class App extends JFrame implements ActionListener {
 
     public App() {
         super("Datos de Estudiantes");
+
         menuBar = new JMenuBar();
         setJMenuBar(menuBar);
         mnuArchivo = new JMenu("Archivo");
@@ -42,12 +43,14 @@ public class App extends JFrame implements ActionListener {
         smnAcercade.addActionListener(this);
 
         jdlAcercaDe = new JDialog(this, "Acerca de ..");
-        jdlAcercaDe.setSize(300, 250);
+        jdlAcercaDe.setSize(325, 150);
         jdlAcercaDe.setLocationRelativeTo(null);
         lblDatos = new JLabel("<html>Programación Oriendada a Objetos I<br>Carlos Castañeda Ramírez</html>",JLabel.CENTER);
         lblDatos.setFont(new Font("Times New Roman", Font.BOLD, 18));
         jdlAcercaDe.add(lblDatos);
+
         setLayout(new GridLayout(2, 1, 0, 0));
+
         pnlTabla = new JPanel();
         getContentPane().add(pnlTabla);
         spane = new JScrollPane();
@@ -56,28 +59,30 @@ public class App extends JFrame implements ActionListener {
         table.getTableHeader().setBackground(Color.yellow);
         table.getTableHeader().setForeground(Color.black);
         spane.setViewportView(table);
+
         modelo = new DefaultTableModel();
         modelo.setColumnIdentifiers(new String[] { "Nombre", "Edad", "Peso", "Correo" });
         table.setModel(modelo);
+
         pnlDatos = new JPanel();
         getContentPane().add(pnlDatos);
         pnlDatos.setLayout(new GridLayout(4, 2, 0, 0));
-        lblNombre = new JLabel("Nombre");
+        lblNombre = new JLabel("Nombre: ");
         lblNombre.setHorizontalAlignment(SwingConstants.RIGHT);
         txtNombre = new JTextField();
         pnlDatos.add(lblNombre);
         pnlDatos.add(txtNombre);
-        lblEdad = new JLabel("Edad");
+        lblEdad = new JLabel("Edad: ");
         lblEdad.setHorizontalAlignment(SwingConstants.RIGHT);
         txtEdad = new JTextField();
         pnlDatos.add(lblEdad);
         pnlDatos.add(txtEdad);
-        lblPeso = new JLabel("Peso");
+        lblPeso = new JLabel("Peso: ");
         lblPeso.setHorizontalAlignment(SwingConstants.RIGHT);
         txtPeso = new JTextField();
         pnlDatos.add(lblPeso);
         pnlDatos.add(txtPeso);
-        lblCorreo = new JLabel("Correo:");
+        lblCorreo = new JLabel("Correo: ");
         lblCorreo.setHorizontalAlignment(SwingConstants.RIGHT);
         txtCorreo = new JTextField();
         pnlDatos.add(lblCorreo);
