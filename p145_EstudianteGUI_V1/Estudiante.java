82c0169883c173eb51676393e95c79b0019c2821
leslieortiz06 <oleslienathali@gmail.com>
2024-11-12 23:45:33 -0600

    actividad 24

---
+++ p145_EstudianteGUI_V1/Estudiante.java
@@ -0,0 +1,50 @@
+package p145_EstudianteGUI_V1;
+
+import java.io.Serializable;
+
+public class Estudiante implements Serializable{
+    String Nombre;
+    int Edad;
+    double Peso;
+    String Correo;
+    
+    public Estudiante(String nombre, int edad, double peso, String correo) {
+        Nombre = nombre;
+        Edad = edad;
+        Peso = peso;
+        Correo = correo;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public int getEdad() {
+        return Edad;
+    }
+    public void setEdad(int edad) {
+        Edad = edad;
+    }
+
+    public double getPeso() {
+        return Peso;
+    }
+    public void setPeso(double peso) {
+        Peso = peso;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+    public void setCorreo(String correo) {
+        Correo = correo;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante [Nombre=" + Nombre + ", Edad=" + Edad + ", Peso=" + Peso + ", Correo=" + Correo + "]";
+    }
+}
